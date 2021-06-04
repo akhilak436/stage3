d06c1e814b66de4f56e8a33148398ef6f8d6bec9
akhilak436 <85303740+akhilak436@users.noreply.github.com>
2021-06-04 11:10:42 +0530

    Add files via upload

---
+++ tphannotation (1)/tbhannotation/src/main/java/com/tbh/tbhannotation/App.java
@@ -0,0 +1,13 @@
+package com.tbh.tbhannotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
