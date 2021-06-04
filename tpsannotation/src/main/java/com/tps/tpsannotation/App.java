389f548d192ec455cc1c496649213248825e9ef7
Akhila reddy <kankanala.akhila123@gmail.com>
2021-06-04 08:00:13 +0530

    Your message about the commit

---
+++ tpsannotation/src/main/java/com/tps/tpsannotation/App.java
@@ -0,0 +1,13 @@
+package com.tps.tpsannotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
