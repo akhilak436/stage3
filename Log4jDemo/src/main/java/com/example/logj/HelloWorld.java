ce0d0a63e87591737c8f4fc0abba45ab93883868
akhilak436 <85303740+akhilak436@users.noreply.github.com>
2021-06-08 11:08:18 +0530

    log4j logging using both xml and properties file

---
+++ Log4jDemo/src/main/java/com/example/logj/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.example.logj;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class HelloWorld {
+    
+	private static final Logger logger = LogManager.getLogger(HelloWorld.class);  
+	 public static void main(String[] args) {  
+	  // basic log4j configurator  
+	  BasicConfigurator.configure();  
+	  logger.info("Hello world");  
+	  logger.info("we are in logger info mode");  
+	  logger.error("this is an error");
+	  logger.fatal("this is fatal");
+	   
+	 }  
+	
+	
+	
+}
